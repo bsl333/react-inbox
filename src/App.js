80a8a444e1cce4c2a8cef93142887a74e8955fdd
Branden <b.lowe333@gmail.com>
2018-08-16 16:15:45 -0700

    toolbar actions stored in a db

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
 // React Comps
 import Toolbar from './components/Toolbar';
@@ -7,6 +8,7 @@ import MessageList from './components/MessageList'
 
 // Message data
 import messages from './data.json';
+const apiUrl = `http://localhost:8082/api`
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +18,7 @@ class App extends Component {
       messages
     }
   }
+
   render() {
     return (
       <div className="container">
@@ -25,17 +28,40 @@ class App extends Component {
     );
   }
 
-  onStarClicked = (id) => {
-    const updatedMessages = this.state.messages.map(message => {
-      if (message.id === id) message.starred = !message.starred
-      return message
-    })
-    this.setState({
-      messages: updatedMessages
-    })
+
+  componentDidMount() {
+    this.getMessages()
   }
 
-  onCheckboxClicked = (id) => {
+  getMessages = async () => {
+    try {
+      const messagesResp = await axios.get(`${apiUrl}/messages`)
+      const messages = messagesResp.data
+      this.setState(prevState => {
+        const updatedMessages = messages.map(message => {
+          message.selected = prevState.messages.find(prevMes => prevMes.id === message.id).selected
+          return message
+        })
+        return {
+          messages: updatedMessages
+        }
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  onStarClicked = async (id) => {
+    try {
+      await axios.patch(`${apiUrl}/messages`, { messageIds: [id], command: 'star' })
+    } catch (e) {
+      console.error(e)
+    } finally {
+      this.getMessages()
+    }
+  }
+
+  onCheckboxClicked = async (id) => {
     const updateMessages = this.state.messages.map(message => {
       if (message.id === id) message.selected = !message.selected
       return message
@@ -63,58 +89,104 @@ class App extends Component {
     }
   }
 
-  onDeleteSelected = () => {
-    this.setState({
-      messages: this.state.messages.filter(message => !message.selected)
-    })
+  onDeleteSelected = async () => {
+    try {
+      const messageIds = this.getSelectedMessagesIds()
+      await axios.patch(`${apiUrl}/messages`, { messageIds, command: 'delete'})
+    } catch (e) {
+      console.error(e)
+    } finally {
+      await this.getMessages()
+    }
+    
+    // this.setState({
+    //   messages: this.state.messages.filter(message => !message.selected)
+    // })
   }
 
-  onMarkSelectedRead = () => {
-    this.setState({
-      messages: this.state.messages.map(message => {
-        message.read = message.selected ? true : message.read
-        return message
-      })
-    })
+  onMarkSelectedRead = async () => {
+    try {
+      const messageIds = this.getSelectedMessagesIds()
+      await axios.patch(`${apiUrl}/messages`, { messageIds, command: 'read', read: true })
+    } catch (e) {
+      console.error(e)
+    } finally {
+      await this.getMessages()
+    }
+
+    // this.setState({
+    //   messages: this.state.messages.map(message => {
+    //     message.read = message.selected ? true : message.read
+    //     return message
+    //   })
+    // })
   }
 
-  onMarkSelectedUnread = () => {
-    this.setState({
-      messages: this.state.messages.map(message => {
-        message.read = message.selected ? false : message.read
-        return message
-      })
-    })
+  onMarkSelectedUnread = async () => {
+    try {
+      const messageIds = this.getSelectedMessagesIds()
+      await axios.patch(`${apiUrl}/messages`, { messageIds, command: 'read', read: false })
+    } catch (e) {
+      console.error(e)
+    } finally {
+      this.getMessages()
+    }
+
+    // this.setState({
+    //   messages: this.state.messages.map(message => {
+    //     message.read = message.selected ? false : message.read
+    //     return message
+    //   })
+    // })
   }
 
-  onSelectedApplyLabel = (e) => {
-    const label = e.target.value
-    if (label === 'Apply label') return
+  onSelectedApplyLabel = async (e) => {
+    try {
+      const label = e.target.value
+      if (label === 'Apply label') return 
+      const messageIds = this.getSelectedMessagesIds()
+      await axios.patch(`${apiUrl}/messages`, { messageIds, command: 'addLabel', label })
+    } catch (e) {
+      console.error(e)
+    } finally {
+      await this.getMessages()
+    }
+    // const label = e.target.value
+    // if (label === 'Apply label') return
 
-    const updatedMessages = this.state.messages.map(message => {
-      if (message.selected && !message.labels.includes(label)) {
-        message.labels = [...message.labels, label]
-      }
-      return message
-    })
+    // const updatedMessages = this.state.messages.map(message => {
+    //   if (message.selected && !message.labels.includes(label)) {
+    //     message.labels = [...message.labels, label]
+    //   }
+    //   return message
+    // })
 
-    this.setState({
-      messages: updatedMessages
-    })
+    // this.setState({
+    //   messages: updatedMessages
+    // })
   }
 
-  onSelectedRemoveLabel = (e) => {
-    const label = e.target.value
-    const updatedMessages = this.state.messages.map(message => {
-      if (message.selected && message.labels.includes(label)) {
-        message.labels = message.labels.filter(val => val !== label)
-      }
-      return message
-    })
+  onSelectedRemoveLabel = async (e) => {
+    try {
+      const label = e.target.value
+      const messageIds = this.getSelectedMessagesIds()
+      await axios.patch(`${apiUrl}/messages`, { messageIds, command: 'removeLabel', label })
+    } catch (e) {
+      console.error(e)
+    } finally {
+      await this.getMessages()
+    }
+    // const label = e.target.value
+    // const updatedMessages = this.state.messages.map(message => {
+    //   if (message.selected && message.labels.includes(label)) {
+    //     message.labels = message.labels.filter(val => val !== label)
+    //   }
+    //   return message
+    // })
 
-    this.setState({
-      messages: updatedMessages
-    })
+    // this.setState({
+    //   messages: updatedMessages
+    // })
   }
 
   calculateUnreadMessages = () => {
@@ -129,6 +201,12 @@ class App extends Component {
 
   noMessagesSelected = () => {
     return this.state.messages.every(message => !message.selected)
+  }
+
+  getSelectedMessagesIds() {
+    return this.state.messages
+      .filter(message => message.selected)
+      .map(message => message.id)
   }
 
 
